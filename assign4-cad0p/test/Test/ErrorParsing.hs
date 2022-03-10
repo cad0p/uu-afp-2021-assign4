3ef048e52f2d622fe9d1eb4cf8d421419555204b
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-03-10 05:26:36 +0100

    more tests on functor parser

--- assign4-cad0p/test/Test/ErrorParsing.hs
+++ assign4-cad0p/test/Test/ErrorParsing.hs
@@ -3,27 +3,48 @@ module Test.ErrorParsing (qcErrPar, huErrPar) where
 
 import           Test.Tasty
 import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck as QC
 
 import           Assign4.ErrorParsing
 
-import           Data.Char            (digitToInt, isDigit)
+import           Data.Char             (digitToInt, isDigit)
 
 
 
 qcErrPar :: TestTree
-qcErrPar = testGroup "ErrPar" []
+qcErrPar = testGroup "ErrPar" [ qcFunParser ]
 
 huErrPar :: TestTree
-huErrPar = testGroup "ErrPar" [ huIntParser ]
+huErrPar = testGroup "ErrPar" [ huIntParser
+                              , huFunParser ]
 
 
 huIntParser :: TestTree
-huIntParser = testGroup "parser int instance"
+huIntParser = testGroup "instance Parser Int"
   [ testCase "101" (
       runParser intSumParser "101"
     @?=
       Right (2, "")
   )]
+
+
+huFunParser :: TestTree
+huFunParser = testGroup "instance Functor Parser"
+  [ testCase "101 (+ 1)" (
+      runParser (fmap (+ 1) intSumParser) "101"
+    @?=
+      Right (3, "")
+  )
+  , testCase "101 (* 3)" (
+      runParser (fmap (* 3) intSumParser) "101"
+    @?=
+      Right (6, "")
+  )
+  ]
+
+qcFunParser :: TestTree
+qcFunParser = testGroup "instance Functor Parser" []
+  -- [ QC.testProperty ""]
 
 
 {-|
